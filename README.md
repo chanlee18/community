b89003df3ad4401285593b4df73043eb0b898757
chanlee <chanlee18@foxmail.com>
2020-03-12 19:44:24 +0800

    extract github config from code to properties

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 ##码农学习日常
 
 ##学习资料获取
-
+[Spring 文档](https://spring.io/guides)
+[Spring Web](http://spring.io/guide/gs/serving-web-content/)
+[es](http://elasticsearch.cn/explore)
+[Github deploy key](https://developer.github.com/v3/guides/managing-deploy-keys/#deploy-keys)
+[bootstrap](https://v3.bootcss.com/getting-started)  
+[Github OAuth](https://developer.github.com/apps/building-oauth-apps/creating-an-oauth-app/)
 ##学习工具
-git
+[Git](https://git-scm.com/download)
+[Visual Paradigm](https://www.visual-paradigm.com)
---
+++ src/main/java/life/asking/community/community/controller/AuthorizeController.java
@@ -0,0 +1,44 @@
+package life.asking.community.community.controller;
+
+import life.asking.community.community.dto.AccessTokenDTO;
+import life.asking.community.community.dto.GithubUser;
+import life.asking.community.community.provider.GithubProvider;
+import org.springframework.beans.factory.annotation.Autowire;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class AuthorizeController {
+
+    @Autowired
+    private GithubProvider githubProvider;
+
+    @Value("${github.client.id}")
+    private String clientId;
+    @Value("${github.client.secret}")
+    private String clientSecret;
+    @Value("${github.redirect.uri}")
+    private String redirectUri;
+
+    @RequestMapping("/callback")
+    public String callback(@RequestParam(name = "code") String code,
+                           @RequestParam(name = "state") String state){
+        AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setClient_secret(clientSecret);
+        accessTokenDTO.setCode(code);
+        accessTokenDTO.setRedirect_uri(redirectUri);
+        accessTokenDTO.setState(state);
+        String accessToken = githubProvider.getAccessToken(accessTokenDTO);
+        GithubUser user = githubProvider.getUser(accessToken);
+
+        //判断user是否为空，否则会提示"空指针异常"（当user为空时，不可调用getName方法）
+        if(null != user){
+            System.out.println(user.getName());
+        }
+        return "index";
+    }
+}
